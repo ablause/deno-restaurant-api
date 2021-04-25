b747a772383726a630008886a1c0b16db0d8ed89
ablause <46499306+ablause@users.noreply.github.com>
2021-04-25 18:10:10 +0200

    fix: Asynchrone validation middleware

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,12 +1,13 @@
-import { ZodSchema } from "https://deno.land/x/zod@v3.0.0-alpha.33/mod.ts";
-import { Context, helpers } from "https://deno.land/x/oak@v7.3.0/mod.ts";
 import { Status } from "https://deno.land/std@0.94.0/http/mod.ts";
+import type { ZodSchema } from "https://deno.land/x/zod@v3.0.0-alpha.33/mod.ts";
+import type { Context } from "https://deno.land/x/oak@v7.3.0/mod.ts";
 
 const validation = (schema: ZodSchema<any>) => {
   return async (context: Context, next: () => void) => {
-    const { value } = context.request.body();
+    const body = await context.request.body()
+      .value;
 
-    schema.parseAsync(value)
+    schema.parseAsync(body)
       .then(() => next())
       .catch((err) => context.throw(Status.BadRequest, err));
   };
